6e048b32f46b95d6e7b71f6fd38b23dba75aa551
tanerizawa <studiomalaka@gmail.com>
2025-06-11 22:27:00 +0700

    Remove duplicate converters

--- app/src/main/java/com/example/diarydepresiku/DiaryDatabase.kt
+++ app/src/main/java/com/example/diarydepresiku/DiaryDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters // **PENTING: Import TypeConverters**
+import com.example.diarydepresiku.Converters
 
 /**
  * DiaryDatabase: Kelas Room Database untuk aplikasi.
